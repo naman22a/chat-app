d94544745303669b8590c28d08aa6185192d499d
Naman Arora <namanarora1022@gmail.com>
2023-07-29 19:38:12 +0530

    fix!: moved to next 12 pages instead of next 13 app

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Index: NextPage = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <button className="btn btn-primary">Click</button>
+        </div>
+    );
+};
+
+export default Index;
