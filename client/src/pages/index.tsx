44db818926723f7117b8635069cbb1c03e6439b2
Naman Arora <namanarora1022@gmail.com>
2023-07-30 12:09:12 +0530

    style: list of users

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -17,23 +17,25 @@ const Index: NextPage = () => {
 
     return (
         <div>
-            <h1 className="text-4xl font-semibold mb-5">List of users</h1>
-            {users.map((user) => (
-                <div key={user.id}>
-                    <div className="avatar online">
-                        <div className="w-32 rounded-full bg-gray-50">
-                            <Image
-                                src={user.avatar}
-                                alt={user.username}
-                                height={100}
-                                width={100}
-                            />
+            <h1 className="text-4xl font-semibold mb-10">List of users</h1>
+            <div className="flex gap-10">
+                {users.map((user) => (
+                    <div key={user.id}>
+                        <div className="avatar online">
+                            <div className="w-32 rounded-full bg-gray-50">
+                                <Image
+                                    src={user.avatar}
+                                    alt={user.username}
+                                    height={100}
+                                    width={100}
+                                />
+                            </div>
                         </div>
+                        <h3>Username: {user.username}</h3>
+                        <h3>Email: {user.email}</h3>
                     </div>
-                    <h3>Username: {user.username}</h3>
-                    <h3>Email: {user.email}</h3>
-                </div>
-            ))}
+                ))}
+            </div>
         </div>
     );
 };
