3d497c63a18375145894984d78a6eb91697e70b6
Naman Arora <namanarora1022@gmail.com>
2023-08-14 21:21:47 +0530

    fix: events ClientToServerEvents

--- client/src/constants/events.ts
+++ client/src/constants/events.ts
@@ -4,10 +4,24 @@ import { User } from '@/api/users/types';
 export interface ServerToClientEvents {
     joined: (payload: Omit<User, 'password'>) => void;
     receiveMessage: (msgText: Message) => void;
+    typingResponse: (payload: { roomName: string; isTyping: boolean }) => void;
 }
 
 export interface ClientToServerEvents {
-    join: (roomName: string) => Omit<User, 'password'>;
-    sendMessage: (message: string, roomId: number) => string;
-    messages: (roomId: number) => Message[];
+    join: (
+        payload: { roomName: string },
+        cb?: () => void,
+    ) => Omit<User, 'password'>;
+    sendMessage: (
+        payload: { message: string; roomName: string },
+        cb?: (newMsg: Message) => void,
+    ) => string;
+    messages: (
+        payload: { roomId: number },
+        cb?: (data: Message[]) => void,
+    ) => Message[];
+    typing: (
+        payload: { name: string; isTyping: boolean },
+        cb?: () => void,
+    ) => void;
 }
--- server/src/common/events/index.ts
+++ server/src/common/events/index.ts
@@ -3,4 +3,5 @@ import { Message, User } from '@prisma/client';
 export interface ServerToClientEvents {
     joined: (payload: Omit<User, 'password'>) => void;
     receiveMessage: (msg: Message) => void;
+    typingResponse: (payload: { name: string; isTyping: boolean }) => void;
 }
